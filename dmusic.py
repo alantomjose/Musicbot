54b4089867f9934708901f76bdfd5acccad01a8c
wilderbe4st <wilderbe4st@users.noreply.github.com>
2017-06-25 20:12:25 +0530

    Update dmusic.py

--- dmusic.py
+++ dmusic.py
@@ -6,13 +6,14 @@ from urllib.request import urlopen
 
 def dload(song,artist):
     print("Searching for %s by %s"%(song,artist))
-    
+    # Search on google for the music
     res=requests.get('https://www.google.co.in/search?q=parent+directory+%s+%s'%(song.replace(' ','+'),artist.replace(' ','+')))
     if res.status_code!=requests.codes.ok:
         return -1
     soup=bs4.BeautifulSoup(res.text,"html.parser")
     elems=soup.select('.s cite')
     n=0
+    # iterate through the results
     while n!= -1:
         
         res2=requests.get('http://%s'%elems[n].getText())
@@ -20,9 +21,11 @@ def dload(song,artist):
             print("happy")
         soup2=bs4.BeautifulSoup(res2.text,"html.parser")
         elems2=soup2.select('a')
+        # iterate through the files to fing the right mp3
         for i in range(0,len(elems2)):
             if song in elems2[i].getText().lower():
                 print(elems2[i].getText()) 
+                # get link to the mp3 file
                 link='http://'+elems[n].getText()+elems2[i].get('href')
                 print(link)
                 openmp3=open('song.mp3','wb')
